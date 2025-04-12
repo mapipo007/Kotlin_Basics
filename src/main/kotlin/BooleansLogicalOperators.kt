94a402575feea683f3285504b2b91669f503e34e
mapipo007 <hatemkarim427@gmail.com>
2025-04-12 20:53:34 +0400

    comparison operator

--- src/main/kotlin/BooleansLogicalOperators.kt
+++ src/main/kotlin/BooleansLogicalOperators.kt
@@ -4,6 +4,7 @@ fun main() {
     val isDriver = true
 
     println(isAdult && isFemale && isDriver)  // both have top be true to have a true result otherwise, its false
+    println(isAdult && (!isFemale && isDriver))
     println(isAdult || isFemale || isDriver)  // at least one has to be true to have a true result.
     println(!isAdult)
 }
---
+++ src/main/kotlin/ComparisonsOperator.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val n1 = 10
+    val n2 = 20
+
+    println(n1 > n2)
+    println(n1 >= n2)
+    println(n1 < n2)
+    println(n1 <= n2)
+    println(n1 == n2)
+}
