9770fa8b634a60fea3497d647284f3f8ed622873
______Eudel Cabrel <hatemkarim427@gmail.com>
2025-04-15 12:22:08 +0400

    Update Const.kt

--- src/main/kotlin/Const.kt
+++ src/main/kotlin/Const.kt
@@ -1,5 +1,8 @@
 fun main() {
     val name = "jamal"  // read only
+    val firtname = "kamal"
+    val lastName = "Amadou"
+    val fullName = "$firstName &lastName"  // read only
     var i = 0 // mutable
 
 }
